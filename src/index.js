cfb41e7c8ab25374a15066cf547ee92221699261
Никита Гавшин <git add>
2020-10-20 12:46:55 +0400

    feat:start task brakets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split("");
+  let myStack = new Array();
+  console.log(str);
+  console.log(bracketsConfig[0][0]);
+  if (str.length < 2) {
+    console.log(false);
+  } else if (str.length % 2 === 1) {
+    console.log(false);
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === bracketsConfig[0][0]) {
+        myStack.push(str[i]);
+        console.log(myStack + "pushed");
+      } else if (myStack.length !== 0 && str[i] === bracketsConfig[0][1]) {
+        myStack.pop(str[i]);
+        console.log(myStack + "popped");
+      } else {
+        return false;
+      }
+    }
+    if (myStack.length === 0) {
+      return true;
+    }
+  }
 }
